a407cad009369f29c6ce8289fb463de9b2efa31a
JohnyLib <perevalov.johny@gmail.com>
2025-12-19 22:31:40 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const locales = ["ru", "ro", "en"] as const;
-const defaultLocale = "ru";
+const defaultLocale = "en";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
